8e3fdb0e3e08d9de60f928929d65615ede991dc5
leobarrientos02 <leobarrientos02@gmail.com>
2022-07-07 14:44:42 -0400

    Linking Stripe and Auth0

--- frontend/pages/api/stripe.js
+++ frontend/pages/api/stripe.js
@@ -1,14 +1,22 @@
 import Stripe from "stripe";
 
 const stripe = new Stripe(`${process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY}`);
+import { getSession } from "@auth0/nextjs-auth0";
 
 export default async function handler(req, res) {
+
+  const session = getSession(req,res);
+  const user = session?.user;
+  // console.log(user);
+  const stripeId = user['http://localhost:3000/stripe_customer_id'];
+
   if (req.method === "POST") {
     try {
       // Create Checkout Sessions from body params.
       const session = await stripe.checkout.sessions.create({
         submit_type: "pay",
         mode: "payment",
+        customer: stripeId,
         payment_method_types: ["card"],
         shipping_address_collection: {
           allowed_countries: ["US", "CA", "GB", "MX", "ES", "RU", "RO", "GB", "DE"],
