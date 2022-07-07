0c5c0b439a62c62e90ba09c9439c3dc6a48fe45a
leobarrientos02 <leobarrientos02@gmail.com>
2022-07-07 14:37:21 -0400

    Google Sign in functionality

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -3,15 +3,22 @@ import { FiShoppingBag } from "react-icons/fi";
 import { NavStyles, NavItems } from "../styles/Nav";
 import Cart from "./Cart";
 import { useStateContext } from "../lib/context";
+import User from "./User";
+import { useUser } from "@auth0/nextjs-auth0"; // Check if user is already logged in
 
 const {AnimatePresence, motion } = require("framer-motion");
 
 export default function Nav(){
     const {showCart, setShowCart, totalQuantity} = useStateContext();
+
+    const {user, error, isLoading} = useUser(); // Check if user is already logged in
+    console.log(user);
+
     return(
         <NavStyles>
             <Link href={'/'}>Apple Tech.</Link>
             <NavItems>
+                <User />
                 <div onClick={() => setShowCart(true)} title="View Cart">
                     {totalQuantity > 0 && 
                         <motion.span animate={{scale: 1}} initial={{scale: 0}}>
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,18 +2,21 @@ import "../styles/globals.css";
 import { Provider, createClient } from "urql";
 import Nav from "../components/Nav";
 import { StateContext } from "../lib/context";
+import { UserProvider } from "@auth0/nextjs-auth0";
 
 // Fetching API
 const client = createClient({ url: process.env.NEXT_PUBLIC_BACKEND_API });
 
 function MyApp({ Component, pageProps }) {
   return (
-    <StateContext children={""}>
-      <Provider value={client}>
-        <Nav />
-        <Component {...pageProps} />
-      </Provider>
-    </StateContext>
+    <UserProvider>
+      <StateContext children={""}>
+        <Provider value={client}>
+          <Nav />
+          <Component {...pageProps} />
+        </Provider>
+      </StateContext>
+    </UserProvider>
   );
 }
 
